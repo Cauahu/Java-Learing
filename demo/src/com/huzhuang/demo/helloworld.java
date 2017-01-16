934ab241d2315b435a0a3a74475395dc82e27e80
cauahu <andahuzhuang@163.com>
2017-01-16 20:19:02 +0800

    add demo

---
+++ demo/src/com/huzhuang/demo/helloworld.java
@@ -0,0 +1,11 @@
+package com.huzhuang.demo;
+
+/**
+ * Created by cauahu on 17-1-16.
+ */
+public class helloworld {
+    public static void main(String argc[])
+    {
+        System.out.println("hello world" );
+    }
+}
