75f1656d0ad758b54a6f50e419aa47567111330e
cauahu <andahuzhuang@163.com>
2017-01-18 11:25:34 +0800

    修改demo

--- demo/src/com/huzhuang/demo/helloworld.java
+++ demo/src/com/huzhuang/demo/helloworld.java
@@ -1,11 +1,22 @@
 package com.huzhuang.demo;
 
+import java.util.Scanner;
+
 /**
  * Created by cauahu on 17-1-16.
  */
 public class helloworld {
     public static void main(String argc[])
     {
-        System.out.println("hello world" );
+        //System.out.println("hello world" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("pleace input your own text: " );
+        while (true)
+        {
+            String line = scanner.nextLine();
+            if(line.equals("exit"))
+                break;
+            System.out.println(">>>" + line);
+        }
     }
 }
