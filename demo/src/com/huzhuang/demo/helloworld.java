1b197a1e51b133b1e858dfe84f29483b5e813cee
cauahu <andahuzhuang@163.com>
2017-04-17 14:24:22 +0800

    demo modify

--- demo/src/com/huzhuang/demo/helloworld.java
+++ demo/src/com/huzhuang/demo/helloworld.java
@@ -8,6 +8,19 @@ import java.util.Scanner;
 public class helloworld {
     public static void main(String argc[])
     {
+        /*for(int i=0;i<6;i++)
+            System.out.println(Math.random());
+        for(int i=0;i<6;i++)
+            System.out.println(Math.random());
+        int i = 3, j = 3;
+        if(i)
+        {
+            System.out.println("i equals j");
+        }
+        else
+        {
+            System.out.println("i not equals j");
+        }*/
         //System.out.println("hello world" );
         Scanner scanner = new Scanner(System.in);
         System.out.println("pleace input your own text: " );
